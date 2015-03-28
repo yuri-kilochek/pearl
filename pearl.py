dafc2dd254dc7fda501087049c1926af540cc268
yuri_kilochek <yuri.kilochek@gmail.com>
2015-03-29 00:34:20 +0300

    Implemented (yet again) grammar specification EDSL.

--- pearl.py
+++ pearl.py
@@ -1,76 +1,114 @@
 from collections import defaultdict as _defaultdict
 from itertools import chain as _chain
+from functools import wraps as _wraps
+from inspect import getfullargspec as _getfullargspec
 
 
-def default_transform(grammar, *results):
-    return [grammar] + list(results)
-
-
-class _Rule:
-    def __init__(self, head, body, transform):
-        self.__head = head
-        self.__body = body
-        self.__transform = transform
-
-    @property
-    def __key(self):
-        return self.__head, self.__body
-
-    def __hash__(self):
-        return hash(self.__key)
-
-    def __eq__(self, other):
-        return self.__key == other.__key
-
-    @property
-    def head(self):
-        return self.__head
-
-    @property
-    def body(self):
-        return self.__body
-
-    @property
-    def transform(self):
-        return self.__transform
-
-
-class Grammar:
-    def __init__(self, rules=()):
-        self.__rule_groups = _defaultdict(dict)
+class _GrammarMeta(type):
+    def __getitem__(cls, rules):
+        if type(rules) != tuple:
+            rules = rules,
+        grammar = cls()
         for rule in rules:
-            assert type(rule) == _Rule
-            self.__rule_groups[rule.head][rule.body] = rule
+            assert type(rule) == slice
+            grammar = grammar.put(rule.start, rule.stop, rule.step)
+        return grammar
+
+
+def default_action(*args):
+    if not args:
+        return None
+    assert len(args) == 1
+    return args[0]
+
+
+class Grammar(metaclass=_GrammarMeta):
+    class Rule:
+        def __init__(self, head, body, action):
+            assert type(head) == str and head
+            self.__head = head
+
+            assert type(body) == list
+            _body = []
+            retained = set()
+            for i, element in enumerate(body):
+                if type(element) == set:
+                    element = element.pop()
+                    retained.add(i)
+                assert type(element) == str and element
+                _body.append(element)
+            self.__body = tuple(_body)
+            retained = frozenset(retained)
+
+            if action is None:
+                action = default_action
+            assert callable(action)
+            def build_action():
+                argspec = _getfullargspec(action)
+                @_wraps(action)
+                def raw_action(values, tokens, grammar):
+                    args = [v for i, v in enumerate(values) if i in retained]
+                    kwargs = {}
+                    if '_tokens_' in argspec.kwonlyargs:
+                        kwargs['_tokens_'] = tokens
+                    if '_grammar_' in argspec.kwonlyargs:
+                        kwargs['_grammar_'] = grammar
+                    result = action(*args, **kwargs)
+                    if '_grammar_' in argspec.kwonlyargs:
+                        result, grammar = result
+                    return result, grammar
+                return raw_action
+            self.__action = build_action()
+
+        @property
+        def head(self):
+            return self.__head
+
+        @property
+        def body(self):
+            return self.__body
+
+        @property
+        def action(self):
+            return self.__action
+
+    def __init__(self, rules=()):
+        rule_sets = _defaultdict(set)
+        for rule in rules:
+            assert type(rule) == Grammar.Rule
+            rule_sets[rule.head].add(rule)
+        self.__rule_sets = {h: frozenset(rs) for h, rs in rule_sets.items()}
+
         self.__nullables = None
 
     def __getitem__(self, head):
         assert type(head) == str and head
-        return self.__rule_groups[head].values()
+        return self.__rule_sets.get(head, frozenset())
 
-    def put(self, head, body, transform=default_transform):
-        assert type(head) == str
-        assert type(body) == list and all(type(s) == str and s for s in body)
-        assert callable(transform)
-        old = (r for rg in self.__rule_groups.values() for r in rg.values())
-        new = [_Rule(head, tuple(body), transform)]
+    def put(self, head, body, action=default_action):
+        old = (r for rs in self.__rule_sets.values() for r in rs)
+        new = [Grammar.Rule(head, body, action)]
         return Grammar(_chain(old, new))
+
+    def drop(self, head):
+        return Grammar(r for rs in self.__rule_sets.values() for r in rs if r.head != head)
 
     def is_terminal(self, symbol):
         assert type(symbol) == str and symbol
-        return symbol not in self.__rule_groups or not self.__rule_groups[symbol]
+        return symbol not in self.__rule_sets
 
     def is_nullable(self, symbol):
         assert type(symbol) == str and symbol
 
         if self.__nullables is None:
-            self.__nullables = {s for s, rg in self.__rule_groups.items() if () in rg}
-            new_nullable = self.__nullables
+            self.__nullables = {h for h, rs in self.__rule_sets.items() if any(not r.body for r in rs)}
+            new_nullable = bool(self.__nullables)
             while new_nullable:
                 new_nullable = False
-                for head, rule_group in self.__rule_groups.items():
+                for head, rule_set in self.__rule_sets.items():
                     if head in self.__nullables:
                         continue
-                    if any(all(s in self.__nullables for s in b) for b in rule_group):
+                    if any(all(s in self.__nullables for s in r.body) for r in rule_set):
                         self.__nullables.add(head)
                         new_nullable = True
 
@@ -78,16 +116,16 @@ class Grammar:
 
 
 class _Item:
-    def __init__(self, grammar, rule, parents=frozenset(), results=(), progress=0):
+    def __init__(self, grammar, rule, parents=frozenset(), values=(), tokens=()):
         self.__grammar = grammar
         self.__rule = rule
         self.__parents = parents
-        self.__results = results
-        self.__progress = progress
+        self.__values = values
+        self.__tokens = tokens
 
     @property
     def __key(self):
-        return id(self.__grammar), self.__rule, id(self.__parents), self.__results, self.__progress
+        return self.__grammar, self.__rule, id(self.__parents), self.__values
 
     def __hash__(self):
         return hash(self.__key)
@@ -109,21 +147,21 @@ class _Item:
 
     @property
     def is_complete(self):
-        return self.__progress == len(self.__rule.body)
+        return len(self.__values) == len(self.__rule.body)
 
     @property
     def expected_symbol(self):
         assert not self.is_complete
-        return self.__rule.body[self.__progress]
+        return self.__rule.body[len(self.__values)]
 
-    def get_next(self, grammar, results):
+    def get_next(self, grammar, value, tokens):
         assert not self.is_complete
-        return _Item(grammar, self.__rule, self.__parents, self.__results + results, self.__progress + 1)
+        return _Item(grammar, self.__rule, self.__parents, self.__values + (value,), self.__tokens + tokens)
 
     def finalize(self):
         assert self.is_complete
-        grammar, *results = self.__rule.transform(self.__grammar, *self.__results)
-        return grammar, tuple(results)
+        result, grammar = self.__rule.action(self.__values, self.__tokens, self.__grammar)
+        return result, self.__tokens, grammar
 
 
 class _State:
@@ -162,15 +200,16 @@ class ParseError(Exception):
 _END = object()
 
 
-def _parse(grammar, tokens, **settings):
-    match = settings.pop('match')
-    allow_partial = settings.pop('allow_partial')
+def default_match(token, terminal):
+    if token == terminal:
+        return token
+    return None
 
-    assert not settings, 'Unknown settings: ' + ', '.join(settings)
 
+def parse(grammar, tokens, *, match=default_match, allow_partial=False):
     state = _State()
 
-    for rule in grammar['__start__']:
+    for rule in grammar['_start_']:
         state.put(_Item(grammar, rule))
 
     for token in _chain(tokens, [_END]):
@@ -178,31 +217,24 @@ def _parse(grammar, tokens, **settings):
 
         for item in state:
             if item.is_complete:
-                grammar, results = item.finalize()
+                result, tokens, grammar = item.finalize()
                 if item.parents:
                     for parent_item in item.parents:
-                        state.put(parent_item.get_next(grammar, results))
+                        state.put(parent_item.get_next(grammar, result, tokens))
                 else:
                     if allow_partial or token is _END:
-                        yield results
+                        yield result
             elif not item.grammar.is_terminal(item.expected_symbol):
                 if item.grammar.is_nullable(item.expected_symbol):
-                    state.put(item.get_next(item.grammar, ()))
+                    state.put(item.get_next(item.grammar, None, ()))
                 for rule in item.grammar[item.expected_symbol]:
                     state.put(_Item(item.grammar, rule, state[item.expected_symbol]))
             elif token is not _END:
                 result = match(token, item.expected_symbol)
                 if result is not None:
-                    next_state.put(item.get_next(item.grammar, (result,)))
+                    next_state.put(item.get_next(item.grammar, result, (token,)))
 
         if not next_state and token is not _END:
             raise ParseError()
 
         state = next_state
-
-
-def parse(grammar, tokens, **settings):
-    settings.setdefault('match', lambda token, terminal: token if token == terminal else None)
-    settings.setdefault('allow_partial', False)
-
-    return _parse(grammar, tokens, **settings)
--- test.py
+++ test.py
@@ -1,66 +1,64 @@
 import pearl
 
 
-def join(items):
-    if isinstance(items, str):
-        return items
-    return ''.join(map(join, items))
+def ident(fn):
+    def wrapper(*args, **kwargs):
+        return fn(*args, **kwargs)
+    return wrapper
+
 
 def simple_math(**c):
-    g = pearl.Grammar()
+    return pearl.Grammar[
+        '_start_': [{'expr'}],
 
-    g = g.put('__start__', ['expr'])
+        'expr': [{'add/sub'}],
 
-    g = g.put('expr', ['add/sub'])
+        'add/sub': [{'add/sub'}, '+', {'mul/div'}]: ident(c['add']),
+        'add/sub': [{'add/sub'}, '-', {'mul/div'}]: ident(c['sub']),
+        'add/sub': [{'mul/div'}],
 
-    g = g.put('add/sub', ['add/sub', '+', 'mul/div'], lambda g, x, op, y: [g, c['add'](x, y)])
-    g = g.put('add/sub', ['add/sub', '-', 'mul/div'], lambda g, x, op, y: [g, c['sub'](x, y)])
-    g = g.put('add/sub', ['mul/div'])
+        'mul/div': [{'mul/div'}, '*', {'pref'}]: ident(c['mul']),
+        'mul/div': [{'mul/div'}, '/', {'pref'}]: ident(c['div']),
+        'mul/div': [{'pref'}],
 
-    g = g.put('mul/div', ['mul/div', '*', 'pref'], lambda g, x, op, y: [g, c['mul'](x, y)])
-    g = g.put('mul/div', ['mul/div', '/', 'pref'], lambda g, x, op, y: [g, c['div'](x, y)])
-    g = g.put('mul/div', ['pref'])
+        'pref': ['+', {'prim'}]: ident(c['pos']),
+        'pref': ['-', {'prim'}]: ident(c['neg']),
+        'pref': [{'prim'}],
 
-    g = g.put('pref', ['+', 'prim'], lambda g, op, x: [g, c['pos'](x)])
-    g = g.put('pref', ['-', 'prim'], lambda g, op, x: [g, c['neg'](x)])
-    g = g.put('pref', ['prim'])
+        'prim': ['(', {'expr'}, ')'],
+        'prim': [{'num'}],
 
-    g = g.put('prim', ['(', 'expr', ')'], lambda g, lp, x, rp: [g, x])
-    g = g.put('prim', ['num'])
+        'num': ['sgn?', 'int', 'frac?', 'exp?']: lambda *, _tokens_: c['num'](''.join(_tokens_)),
 
+        'int': ['dig', 'int'],
+        'int': ['dig'],
 
-    g = g.put('num', ['sgn?', 'int', 'frac?', 'exp?'], lambda g, *cs: [g, c['num'](join(cs))])
+        'frac?': ['.', 'int'],
+        'frac?': [],
 
-    g = g.put('int', ['dig', 'int'])
-    g = g.put('int', ['dig'])
+        'exp?': ['eE', 'sgn?', 'int'],
+        'exp?': [],
 
-    g = g.put('frac?', ['.', 'int'])
-    g = g.put('frac?', [])
+        'eE': ['e'],
+        'eE': ['E'],
 
-    g = g.put('exp?', ['eE', 'sgn?', 'int'])
-    g = g.put('exp?', [])
+        'sgn?': ['sgn'],
+        'sgn?': [],
 
-    g = g.put('eE', ['e'])
-    g = g.put('eE', ['E'])
+        'sgn': ['+'],
+        'sgn': ['-'],
 
-    g = g.put('sgn?', ['sgn'])
-    g = g.put('sgn?', [])
-
-    g = g.put('sgn', ['+'])
-    g = g.put('sgn', ['-'])
-
-    g = g.put('dig', ['0'])
-    g = g.put('dig', ['1'])
-    g = g.put('dig', ['2'])
-    g = g.put('dig', ['3'])
-    g = g.put('dig', ['4'])
-    g = g.put('dig', ['5'])
-    g = g.put('dig', ['6'])
-    g = g.put('dig', ['7'])
-    g = g.put('dig', ['8'])
-    g = g.put('dig', ['9'])
-
-    return g
+        'dig': ['0'],
+        'dig': ['1'],
+        'dig': ['2'],
+        'dig': ['3'],
+        'dig': ['4'],
+        'dig': ['5'],
+        'dig': ['6'],
+        'dig': ['7'],
+        'dig': ['8'],
+        'dig': ['9'],
+    ]
 
 sexpress = simple_math(
     add='(+ {} {})'.format,
@@ -85,165 +83,157 @@ evaluate = simple_math(
 
 expression = '3*(1/2)'
 
-for r, in pearl.parse(sexpress, expression):
+for r in pearl.parse(sexpress, expression):
     print(r)  # (* 3 (/ 1 2))
 
-for r, in pearl.parse(evaluate, expression):
+for r in pearl.parse(evaluate, expression):
     print(r)  # 1.5
 
 
-def ambiguous():
-    g = pearl.Grammar()
+ambiguous = pearl.Grammar[
+    '_start_': [{'S'}],
 
-    g = g.put('__start__', ['S'])
+    'S': [{'S'}, '+', {'S'}]: ident('({} + {})'.format),
+    'S': [{'a'}],
+]
 
-    g = g.put('S', ['S', '+', 'S'], lambda g, x, op, y: [g, '({} + {})'.format(x, y)])
-    g = g.put('S', ['a'])
-
-    return g
-
-for r, in pearl.parse(ambiguous(), 'a+a+a+a'):
+for r in pearl.parse(ambiguous, 'a+a+a+a'):
     print(r)
 
 
-def dynamic():
-    g = pearl.Grammar()
+dynamic = pearl.Grammar[
+    '_start_': ['actions'],
 
-    g = g.put('__start__', ['actions'])
+    'actions': [],
+    'actions': ['action', 'actions'],
 
-    g = g.put('actions', [])
-    g = g.put('actions', ['action', 'actions'])
+    'action': ['define-action'],
 
-    g = g.put('action', ['define-action'])
+    'define-action': ['!', {'char'}]: lambda c, *, _grammar_: (None, _grammar_.put('action', [c])),
 
-    g = g.put('define-action', ['!', 'char'], lambda g, _, c: [g.put('action', [c]), '!', c])
+    'char': [{'a'}],
+    'char': [{'b'}],
+    'char': [{'c'}],
+    'char': [{'d'}],
+    'char': [{'e'}],
+    'char': [{'f'}],
+    'char': [{'g'}],
+    'char': [{'h'}],
+    'char': [{'i'}],
+    'char': [{'j'}],
+    'char': [{'k'}],
+    'char': [{'l'}],
+    'char': [{'m'}],
+    'char': [{'n'}],
+    'char': [{'o'}],
+    'char': [{'p'}],
+    'char': [{'q'}],
+    'char': [{'r'}],
+    'char': [{'s'}],
+    'char': [{'t'}],
+    'char': [{'u'}],
+    'char': [{'v'}],
+    'char': [{'w'}],
+    'char': [{'x'}],
+    'char': [{'y'}],
+    'char': [{'z'}],
+]
 
-    g = g.put('char', ['a'])
-    g = g.put('char', ['b'])
-    g = g.put('char', ['c'])
-    g = g.put('char', ['d'])
-    g = g.put('char', ['e'])
-    g = g.put('char', ['f'])
-    g = g.put('char', ['g'])
-    g = g.put('char', ['h'])
-    g = g.put('char', ['i'])
-    g = g.put('char', ['j'])
-    g = g.put('char', ['k'])
-    g = g.put('char', ['l'])
-    g = g.put('char', ['m'])
-    g = g.put('char', ['n'])
-    g = g.put('char', ['o'])
-    g = g.put('char', ['p'])
-    g = g.put('char', ['q'])
-    g = g.put('char', ['r'])
-    g = g.put('char', ['s'])
-    g = g.put('char', ['t'])
-    g = g.put('char', ['u'])
-    g = g.put('char', ['v'])
-    g = g.put('char', ['w'])
-    g = g.put('char', ['x'])
-    g = g.put('char', ['y'])
-    g = g.put('char', ['z'])
+for r in pearl.parse(dynamic, '!aaaa!bbbbaa'):
+    print('dynamic is okay')
+    break
+else:
+    print('dynamic failed')
 
-    return g
 
-for r in pearl.parse(dynamic(), '!aaaa!bbbbaa'):
+
+var_math = pearl.Grammar[
+    '_start_': [{'expr'}],
+
+    'expr': ['def', ';', {'expr'}],
+    'expr': [{'add/sub'}],
+
+    'def': [{'id'}, '=', {'expr'}]: lambda n, v, *, _grammar_: (None, _grammar_.put('var', [n], lambda: v)),
+
+    'id': ['chs']: lambda *, _tokens_: ''.join(_tokens_),
+
+    'chs': ['ch', 'chs'],
+    'chs': ['ch'],
+
+    'ch': ['a'],
+    'ch': ['b'],
+    'ch': ['c'],
+    'ch': ['d'],
+    'ch': ['e'],
+    'ch': ['f'],
+    'ch': ['g'],
+    'ch': ['h'],
+    'ch': ['i'],
+    'ch': ['j'],
+    'ch': ['k'],
+    'ch': ['l'],
+    'ch': ['m'],
+    'ch': ['n'],
+    'ch': ['o'],
+    'ch': ['p'],
+    'ch': ['q'],
+    'ch': ['r'],
+    'ch': ['s'],
+    'ch': ['t'],
+    'ch': ['u'],
+    'ch': ['v'],
+    'ch': ['w'],
+    'ch': ['x'],
+    'ch': ['y'],
+    'ch': ['z'],
+
+    'add/sub': [{'add/sub'}, '+', {'mul/div'}]: lambda x, y: x + y,
+    'add/sub': [{'add/sub'}, '-', {'mul/div'}]: lambda x, y: x - y,
+    'add/sub': [{'mul/div'}],
+
+    'mul/div': [{'mul/div'}, '*', {'pref'}]: lambda x, y: x * y,
+    'mul/div': [{'mul/div'}, '/', {'pref'}]: lambda x, y: x / y,
+    'mul/div': [{'pref'}],
+
+    'pref': ['+', {'prim'}]: lambda x: +x,
+    'pref': ['-', {'prim'}]: lambda x: -x,
+    'pref': [{'prim'}],
+
+    'prim': ['(', {'expr'}, ')'],
+    'prim': [{'num'}],
+    'prim': [{'var'}],
+
+    'num': ['sgn?', 'int', 'frac?', 'exp?']: lambda *, _tokens_: float(''.join(_tokens_)),
+
+    'int': ['dig', 'int'],
+    'int': ['dig'],
+
+    'frac?': ['.', 'int'],
+    'frac?': [],
+
+    'exp?': ['eE', 'sgn?', 'int'],
+    'exp?': [],
+
+    'eE': ['e'],
+    'eE': ['E'],
+
+    'sgn?': ['sgn'],
+    'sgn?': [],
+
+    'sgn': ['+'],
+    'sgn': ['-'],
+
+    'dig': ['0'],
+    'dig': ['1'],
+    'dig': ['2'],
+    'dig': ['3'],
+    'dig': ['4'],
+    'dig': ['5'],
+    'dig': ['6'],
+    'dig': ['7'],
+    'dig': ['8'],
+    'dig': ['9'],
+]
+
+for r in pearl.parse(var_math, 'a=2;b=a*a;a+b+1'):
     print(r)
-
-
-
-def var_math():
-    g = pearl.Grammar()
-
-    g = g.put('__start__', ['prog'])
-
-    g = g.put('prog', ['def', ';', 'prog'], lambda g, _, v: [g, v])
-    g = g.put('prog', ['expr'])
-
-    g = g.put('def', ['id', '=', 'expr'], lambda g, n, _, v: [g.put('var', [n], lambda g, _: (g, v))])
-
-    g = g.put('id', ['chs'], lambda g, *cs: [g, join(cs)])
-
-    g = g.put('chs', ['ch', 'chs'])
-    g = g.put('chs', ['ch'])
-
-    g = g.put('ch', ['a'])
-    g = g.put('ch', ['b'])
-    g = g.put('ch', ['c'])
-    g = g.put('ch', ['d'])
-    g = g.put('ch', ['e'])
-    g = g.put('ch', ['f'])
-    g = g.put('ch', ['g'])
-    g = g.put('ch', ['h'])
-    g = g.put('ch', ['i'])
-    g = g.put('ch', ['j'])
-    g = g.put('ch', ['k'])
-    g = g.put('ch', ['l'])
-    g = g.put('ch', ['m'])
-    g = g.put('ch', ['n'])
-    g = g.put('ch', ['o'])
-    g = g.put('ch', ['p'])
-    g = g.put('ch', ['q'])
-    g = g.put('ch', ['r'])
-    g = g.put('ch', ['s'])
-    g = g.put('ch', ['t'])
-    g = g.put('ch', ['u'])
-    g = g.put('ch', ['v'])
-    g = g.put('ch', ['w'])
-    g = g.put('ch', ['x'])
-    g = g.put('ch', ['y'])
-    g = g.put('ch', ['z'])
-
-    g = g.put('expr', ['add/sub'])
-
-    g = g.put('add/sub', ['add/sub', '+', 'mul/div'], lambda g, x, _, y: [g, x + y])
-    g = g.put('add/sub', ['add/sub', '-', 'mul/div'], lambda g, x, _, y: [g, x - y])
-    g = g.put('add/sub', ['mul/div'])
-
-    g = g.put('mul/div', ['mul/div', '*', 'pref'], lambda g, x, _, y: [g, x * y])
-    g = g.put('mul/div', ['mul/div', '/', 'pref'], lambda g, x, _, y: [g, x / y])
-    g = g.put('mul/div', ['pref'])
-
-    g = g.put('pref', ['+', 'prim'], lambda g, _, x: [g, +x])
-    g = g.put('pref', ['-', 'prim'], lambda g, _, x: [g, -x])
-    g = g.put('pref', ['prim'])
-
-    g = g.put('prim', ['(', 'expr', ')'], lambda g, _0, x, _1: [g, x])
-    g = g.put('prim', ['num'])
-    g = g.put('prim', ['var'])
-
-    g = g.put('num', ['sgn?', 'int', 'frac?', 'exp?'], lambda g, *cs: [g, float(join(cs))])
-
-    g = g.put('int', ['dig', 'int'])
-    g = g.put('int', ['dig'])
-
-    g = g.put('frac?', ['.', 'int'])
-    g = g.put('frac?', [])
-
-    g = g.put('exp?', ['eE', 'sgn?', 'int'])
-    g = g.put('exp?', [])
-
-    g = g.put('eE', ['e'])
-    g = g.put('eE', ['E'])
-
-    g = g.put('sgn?', ['sgn'])
-    g = g.put('sgn?', [])
-
-    g = g.put('sgn', ['+'])
-    g = g.put('sgn', ['-'])
-
-    g = g.put('dig', ['0'])
-    g = g.put('dig', ['1'])
-    g = g.put('dig', ['2'])
-    g = g.put('dig', ['3'])
-    g = g.put('dig', ['4'])
-    g = g.put('dig', ['5'])
-    g = g.put('dig', ['6'])
-    g = g.put('dig', ['7'])
-    g = g.put('dig', ['8'])
-    g = g.put('dig', ['9'])
-
-    return g
-
-for r, in pearl.parse(var_math(), 'a=2;b=a*a;a+b+1'):
-    print(r)  # (* 3 (/ 1 2))
